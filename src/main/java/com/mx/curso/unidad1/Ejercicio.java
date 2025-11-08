46e03f7a30b667123c7dff0a3297bcd34ad37dad
ElKazarian <jjhm_10@hotmail.com>
2025-11-08 10:05:38 -0600

    Primer commit

---
+++ src/main/java/com/mx/curso/unidad1/Ejercicio.java
@@ -0,0 +1,7 @@
+package com.mx.curso.unidad1;
+
+public class Ejercicio {
+    static void main(String[] args) {
+        System.out.println("Hola Mundo desde Java");
+    }
+}
---
+++ src/main/java/com/mx/curso/unidad2/secuencia/control/EjercicioFor.java
@@ -0,0 +1,4 @@
+package com.mx.curso.unidad2.secuencia.control;
+
+public class EjercicioFor {
+}
